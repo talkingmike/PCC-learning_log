f47489ea19104aea24b15f4dc95a114e22c30908
Mike Foy <michaelfoy01@hotmail.com>
2017-11-11 15:07:25 -0600

    Update to ALLOWED_HOSTS

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -146,7 +146,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     
     # Only allow heroku to host the project.
-    ALLOWED_HOSTS = ['learning-log-final.herokuapp.com']
+    ALLOWED_HOSTS = ['.herokuapp.com']
     DEBUG = False
 
     # Static asset configuration
